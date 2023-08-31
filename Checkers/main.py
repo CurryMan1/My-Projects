4dcedb1fd60233ab2660dbbaa79c048a9b230e21
ToXicRaiDer6 <139069671+ToXicRaiDer6@users.noreply.github.com>
2023-08-31 13:21:30 +0100

    Flappy bird and Django

--- Checkers/main.py
+++ Checkers/main.py
@@ -3,34 +3,48 @@ from PIL import Image, ImageTk
 from pygame import mixer
 
 mixer.init()
-mixer.Channel(1).play(mixer.Sound('background music.mp3'), -1)
+mixer.Channel(0).set_volume(0.5)
+mixer.Channel(0).play(mixer.Sound('background music.mp3'), -1)
+
+root = Tk()
+root.title('Checkers')
+root.iconbitmap('Red Man.ico')
+root.resizable(False, False)
+
+def sleep(t):
+    root.after(t, root.quit)
+    root.mainloop()
 
 class Checkers:
     def __init__(self, root):
         self.chosen_scheme = 0
-        self.colour_schemes = (('#995a13', '#d69f59'), ('black', 'grey'), ('green','white'))
+        self.colour_schemes = (('#995a13', '#d69f59'), ('black', 'grey'), ('green', 'white'))
         self.dark_colours = tuple(s[0] for s in self.colour_schemes)
         self.turn = 0
         self.squares = []
         self.dot_squares = []
         self.hl = []
+        self.hopping = False
 
-        self.blank = ImageTk.PhotoImage(Image.open("Blank.png").resize((100,100)))
+        self.blank = ImageTk.PhotoImage(Image.open("Blank.png").resize((100,100))) #pyimage1
 
-        self.red_man = ImageTk.PhotoImage(Image.open("Red Man.png").resize((100,100)))
-        self.blue_man = ImageTk.PhotoImage(Image.open("Blue Man.png").resize((100,100)))
+        self.red_man = ImageTk.PhotoImage(Image.open("Red Man.png").resize((100,100))) #pyimage2
+        self.blue_man = ImageTk.PhotoImage(Image.open("Blue Man.png").resize((100,100))) #pyimage3
 
-        self.red_king = ImageTk.PhotoImage(Image.open("Red King.png").resize((100,100)))
-        self.blue_king = ImageTk.PhotoImage(Image.open("Blue King.png").resize((100,100)))
+        self.red_king = ImageTk.PhotoImage(Image.open("Red King.png").resize((100,100))) #pyimage4
+        self.blue_king = ImageTk.PhotoImage(Image.open("Blue King.png").resize((100,100))) #pyimage5
 
-        self.selected_red_man = ImageTk.PhotoImage(Image.open("Selected Red Man.png").resize((100,100)))
-        self.selected_blue_man = ImageTk.PhotoImage(Image.open("Selected Blue Man.png").resize((100, 100)))
+        self.selected_red_man = ImageTk.PhotoImage(Image.open("Selected Red Man.png").resize((100,100))) #pyimage6
+        self.selected_blue_man = ImageTk.PhotoImage(Image.open("Selected Blue Man.png").resize((100, 100))) #pyimage7
 
-        self.selected_red_king = ImageTk.PhotoImage(Image.open("Selected Red King.png").resize((100,100)))
-        self.selected_blue_king = ImageTk.PhotoImage(Image.open("Selected Blue King.png").resize((100,100)))
+        self.selected_red_king = ImageTk.PhotoImage(Image.open("Selected Red King.png").resize((100,100))) #pyimage8
+        self.selected_blue_king = ImageTk.PhotoImage(Image.open("Selected Blue King.png").resize((100,100))) #pyimage9
 
-        self.red_dot = ImageTk.PhotoImage(Image.open("Red Dot.png").resize((100, 100)))
-        self.blue_dot = ImageTk.PhotoImage(Image.open("Blue Dot.png").resize((100, 100)))
+        self.red_dot = ImageTk.PhotoImage(Image.open("Red Dot.png").resize((100, 100))) #pyimage10
+        self.blue_dot = ImageTk.PhotoImage(Image.open("Blue Dot.png").resize((100, 100))) #pyimage11
+
+        self.red_pieces = [str(self.red_king), str(self.red_man)]
+        self.blue_pieces = [str(self.blue_king),str(self.blue_man)]
 
         self.board_frame = Frame(root)
 
@@ -47,9 +61,15 @@ class Checkers:
             square.bind('<Button-1>', lambda event, i=i: self.click(event, i))
             self.squares.append(square)
     def click(self, event, i):
+        if self.hopping:
+            self.turn = 1-self.turn
+            self.hopping = False
+
         #unhighlight
         if self.hl != []:
             self.hl[0]['image'] = self.hl[1]
+
+        #sleep(250)
 
         square_img = self.squares[i]['image']
 
@@ -66,84 +86,114 @@ class Checkers:
 
             #red man
             if square_img == str(self.red_man) and self.turn == 0:
-                self.check_red_man(square_img, i)
+                self.check_piece(square_img, i, self.red_dot, self.selected_red_man, self.blue_pieces,-1)
 
             #blue man
             elif square_img == str(self.blue_man) and self.turn == 1:
-                self.check_blue_man(square_img, i)
+                self.check_piece(square_img, i, self.blue_dot, self.selected_blue_man, self.red_pieces)
+
+            #red king
+            elif square_img == str(self.red_king) and self.turn == 0:
+                self.check_piece(square_img, i, self.red_dot, self.selected_red_king, self.blue_pieces, king=True)
+
+            #blue king
+            elif square_img == str(self.blue_king) and self.turn == 1:
+                self.check_piece(square_img, i, self.blue_dot, self.selected_blue_king, self.red_pieces, king=True)
 
             #dots
             elif square_img in [str(self.red_dot), str(self.blue_dot)]:
+                #promote?
+                if self.last_selected['image'] == str(self.red_man) and i in [1, 3, 5, 7]:
+                    self.last_selected['image'] = self.red_king
+                elif self.last_selected['image'] == str(self.blue_man) and i in [56, 58, 60, 62]:
+                    self.last_selected['image'] = self.blue_king
 
                 #move
-                if square_img == str(self.red_dot):
-                    self.last_selected['image'] = self.blank
-                    self.squares[i]['image'] = self.red_man
-                else:
-                    self.last_selected['image'] = self.blank
-                    self.squares[i]['image'] = self.blue_man
+                self.squares[i]['image'] = self.last_selected['image']
+                self.last_selected['image'] = self.blank
 
                 #sound
                 if abs(i - self.squares.index(self.last_selected)) < 10:
-                    mixer.music.load('move-self.mp3')
+                    mixer.Channel(1).play(mixer.Sound('move-self.mp3'))
                 else:
-                    mixer.music.load('capture.mp3')
+                    mixer.Channel(2).play(mixer.Sound('capture.mp3'))
                     #remove counter
                     self.squares[int((i + self.squares.index(self.last_selected))/2)]['image'] = self.blank
 
-                mixer.music.play()
+                    #check for double
+                    if self.squares[i]['image'] == str(self.red_man):
+                        self.check_double(self.squares[i]['image'], i, self.red_dot, self.selected_red_man, self.blue_pieces, -1)
+                    elif self.squares[i]['image'] == str(self.blue_man):
+                        self.check_double(self.squares[i]['image'], i, self.blue_dot, self.selected_blue_man, self.red_pieces, 1)
+                    elif self.squares[i]['image'] == str(self.red_king):
+                        self.check_double(self.squares[i]['image'], i, self.red_dot, self.selected_red_king, self.blue_pieces, king=True)
+                    elif self.squares[i]['image'] == str(self.blue_king):
+                        self.check_double(self.squares[i]['image'], i, self.blue_dot, self.selected_blue_king, self.red_pieces, king=True)
 
                 #next turn
-                self.turn = 1 - self.turn
+                if not self.hopping:
+                    self.turn = 1 - self.turn
+                else:
+                    self.hopping = False
 
         self.last_selected = self.squares[i]
 
-    def check_red_man(self, square_img, i):
+    def check_piece(self, square_img, i, dot, selected, pieces, ml=1, king=False):
         #highlight
-        self.squares[i]['image'] = self.selected_red_man
-        self.hl = [self.squares[i],self.red_man]
+        self.squares[i]['image'] = selected
+        self.hl = [self.squares[i], square_img]
         #check left square
-        if self.squares[i-9]['image'] == str(self.blank) and self.squares[i-9]['bg'] in self.dark_colours:
-            self.squares[i-9]['image'] = self.red_dot
-            self.dot_squares.append(self.squares[i-9])
-        elif self.squares[i-9]['image'] == str(self.blue_man) and self.squares[i-18]['image'] == str(self.blank)\
-                and self.squares[i-18]['bg'] in self.dark_colours:
-            self.squares[i-18]['image'] = self.red_dot
-            self.dot_squares.append(self.squares[i - 18])
+        if i + (ml * 9) in range(64):
+            if self.squares[i+(ml*9)]['image'] == str(self.blank) and self.squares[i+(ml*9)]['bg'] in self.dark_colours:
+                self.squares[i+(ml*9)]['image'] = dot
+                self.dot_squares.append(self.squares[i+(ml*9)])
+            elif i + (ml * 18) in range(64):
+                if self.squares[i+(ml*9)]['image'] in pieces and self.squares[i+(ml*18)]['image'] == str(self.blank)\
+                    and self.squares[i+(ml*18)]['bg'] in self.dark_colours:
+                    self.squares[i+(ml*18)]['image'] = dot
+                    self.dot_squares.append(self.squares[i+(ml*18)])
         #check right square
-        if self.squares[i-7]['image'] == str(self.blank) and self.squares[i-7]['bg'] in self.dark_colours:
-            self.squares[i-7]['image'] = self.red_dot
-            self.dot_squares.append(self.squares[i - 7])
-        elif self.squares[i-7]['image'] == str(self.blue_man) and self.squares[i-14]['image'] == str(self.blank)\
-                and self.squares[i - 14]['bg'] in self.dark_colours:
-            self.squares[i-14]['image'] = self.red_dot
-            self.dot_squares.append(self.squares[i-14])
+        if i + (ml * 7) in range(64):
+            if self.squares[i+(ml*7)]['image'] == str(self.blank) and self.squares[i+(ml*7)]['bg'] in self.dark_colours:
+                self.squares[i+(ml*7)]['image'] = dot
+                self.dot_squares.append(self.squares[i+(ml*7)])
+            elif i + (ml * 14) in range(64):
+                if self.squares[i+(ml*7)]['image'] in pieces and self.squares[i+(ml*14)]['image'] == str(self.blank)\
+                    and self.squares[i+(ml*14)]['bg'] in self.dark_colours:
+                    self.squares[i+(ml*14)]['image'] = dot
+                    self.dot_squares.append(self.squares[i+(ml*14)])
 
-    def check_blue_man(self, square_img, i):
-        #highlight
-        self.squares[i]['image'] = self.selected_blue_man
-        self.hl = [self.squares[i],self.blue_man]
+        #if king repeat
+        if king:
+            self.check_piece(square_img, i, dot, selected, pieces, ml * (-1))
 
+    def check_double(self, square_img, i, dot, selected, pieces, ml=1, king=False):
+        self.hopping=False
         #check left square
-        if self.squares[i+9]['image'] == str(self.blank) and self.squares[i+9]['bg'] in self.dark_colours:
-            self.squares[i+9].config(image=self.blue_dot)
-            self.dot_squares.append(self.squares[i+9])
-        elif self.squares[i+9]['image'] == str(self.red_man) and self.squares[i+18]['image'] == str(self.blank)\
-                and self.squares[i+18]['bg'] in self.dark_colours:
-            self.squares[i+18]['image'] = self.blue_dot
-            self.dot_squares.append(self.squares[i+18])
-        #check right square
-        if self.squares[i + 7]['image'] == str(self.blank) and self.squares[i + 7]['bg'] in self.dark_colours:
-            self.squares[i + 7].config(image=self.blue_dot)
-            self.dot_squares.append(self.squares[i + 7])
-        elif self.squares[i + 7]['image'] == str(self.red_man) and self.squares[i + 14]['image'] == str(self.blank)\
-                and self.squares[i + 14]['bg'] in self.dark_colours:
-            self.squares[i + 14]['image'] = self.blue_dot
-            self.dot_squares.append(self.squares[i + 14])
+        if i + (ml * 18) in range(64):
+            if self.squares[i+(ml*9)]['image'] in pieces and self.squares[i+(ml*18)]['image'] == str(self.blank)\
+                    and self.squares[i+(ml*18)]['bg'] in self.dark_colours:
+                self.squares[i+(ml*18)]['image'] = dot
+                self.dot_squares.append(self.squares[i+(ml*18)])
+                self.squares[i]['image'] = selected
+                self.hopping = True
+                self.hl = [self.squares[i], square_img]
 
-root = Tk()
-root.title('Checkers')
-root.resizable(False, False)
+        #check right square
+        if i + (ml * 14) in range(64):
+            if self.squares[i+(ml*7)]['image'] in pieces and self.squares[i+(ml*14)]['image'] == str(self.blank)\
+                    and self.squares[i + (ml*14)]['bg'] in self.dark_colours:
+                self.squares[i+(ml*14)]['image'] = dot
+                self.dot_squares.append(self.squares[i + (ml * 14)])
+                self.squares[i]['image'] = selected
+                self.hopping = True
+                self.hl = [self.squares[i], square_img]
+
+        #if king repeat
+        if king:
+            self.check_piece(square_img, i, dot, selected, pieces, ml * (-1))
+
+
 
 g = Checkers(root)
 g.board_frame.grid()
---
+++ Flappy Bird/main.py
@@ -0,0 +1,213 @@
+import pygame as pg
+from random import randint
+from pygame.locals import *
+
+pg.init()
+clock = pg.time.Clock()
+fps = 60
+
+screen_width = 864
+screen_height = 936
+
+screen = pg.display.set_mode((screen_width, screen_height))
+pg.display.set_caption('Flappy Bird')
+
+#font
+font = pg.font.Font("C:/Users/user/Documents/Hardhik's Stuff/Random Stuff/Fonts/Futura Extra Black font.ttf", 60)
+
+#colour
+white = (255, 255, 255)
+
+#game vars
+score = 0
+ground_scroll = 0
+speed = 4
+pipe_gap = 150
+pipe_frequency = 1750 #milliseconds
+last_pipe = pg.time.get_ticks() - pipe_frequency
+flying = False
+game_over = False
+passed_pipe = False
+
+#images
+bg = pg.image.load('bg.png')
+ground = pg.image.load('ground.png')
+restart = pg.image.load('restart.png')
+
+pg.display.set_icon(pg.image.load('bird1.png'))
+
+def draw_text(text, font, fg, x, y):
+    img = font.render(text, True, fg)
+    screen.blit(img, (x, y))
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height/2)
+    score = 0
+    return score
+
+#sprites
+class Bird(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Bird, self).__init__()
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        self.flap_cooldown = 5
+        for num in range(1, 4):
+            img = pg.image.load(f'bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.gravity = 0
+        self.clicked = False
+
+    def update(self):
+        #gravity
+        if flying:
+            self.gravity += 0.5
+            if self.gravity > 10:
+                self.gravity = 15
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.gravity)
+
+        if not game_over:
+            #jump
+            if pg.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.gravity = -10
+                self.clicked = True
+            if pg.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+            #animation
+            self.counter += 1
+
+            if self.counter > self.flap_cooldown:
+                self.counter = 0
+                self.index = (self.index + 1)%len(self.images)
+            self.image = self.images[self.index]
+
+            #tilt
+            self.image = pg.transform.rotate(self.images[self.index], -2*(self.gravity))
+        else:
+            self.image = pg.transform.rotate(self.images[self.index], -90)
+
+class Pipe(pg.sprite.Sprite):
+    def __init__(self, x, y, position):
+        super(Pipe, self).__init__()
+        self.image = pg.image.load('pipe.png')
+        self.rect = self.image.get_rect()
+        #position 1 is from the top, -1 is from the bottom
+        if position == 1:
+            self.image = pg.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(pipe_gap/2)]
+        elif position == -1:
+            self.rect.topleft = [x, y + int(pipe_gap/2)]
+
+    def update(self):
+        self.rect.x -= speed
+        if self.rect.right < 0:
+            self.kill()
+
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x, y]
+
+    def draw(self):
+        action = False
+
+        #get mouse pos
+        pos = pg.mouse.get_pos()
+
+        #mouse over button?
+        if self.rect.collidepoint(pos):
+            if pg.mouse.get_pressed()[0] == 1:
+                action = True
+
+        #draw button
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+bird_group = pg.sprite.Group()
+pipe_group = pg.sprite.Group()
+
+flappy = Bird(100, int(screen_height/2))
+bird_group.add(flappy)
+
+button = Button(screen_width // 2 - 50, screen_height // 2 - 100, restart)
+
+run = True
+while run:
+    clock.tick(fps)
+
+    #background
+    screen.blit(bg, (0,0))
+
+    #pipe
+    bird_group.draw(screen)
+    bird_group.update()
+    pipe_group.draw(screen)
+
+    #draw ground
+    screen.blit(ground, (ground_scroll, 768))
+
+    if len(pipe_group) > 0:
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
+        and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right\
+        and not passed_pipe:
+            passed_pipe = True
+        if passed_pipe:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                passed_pipe = False
+
+    draw_text(str(score), font, white, int(screen_width / 2), 20)
+
+    #hit pipe?
+    if pg.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0:
+        game_over = True
+
+    #hit ground?
+    if flappy.rect.bottom > 768:
+        game_over = True
+        flying = False
+
+    if not game_over and flying == True:
+        #new pipes
+        time_now = pg.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            pipe_height = randint(-200, 100)
+            btm_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
+            top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, 1)
+            pipe_group.add(btm_pipe)
+            pipe_group.add(top_pipe)
+            last_pipe = time_now
+
+        #scrolling ground
+        ground_scroll -= speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
+
+        pipe_group.update()
+
+    #check for game over
+    if game_over:
+        clicked = button.draw()
+        if clicked:
+            game_over = False
+            score = reset_game()
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+        if event.type == pg.MOUSEBUTTONDOWN and not flying and not game_over:
+            flying = True
+
+    pg.display.update()
+
+pg.quit()
