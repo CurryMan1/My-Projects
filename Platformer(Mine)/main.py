3a2838311f54a032b1770debd2bae5b6700020e9
ToXicRaiDer6 <139069671+ToXicRaiDer6@users.noreply.github.com>
2023-10-01 15:45:05 +0100

    Platformer Added player animations

--- Platformer(Mine)/main.py
+++ Platformer(Mine)/main.py
@@ -7,6 +7,7 @@ from os.path import join
 
 #files
 from button import Button
+from player import Player
 
 #initialise pygame
 pygame.init()
@@ -34,11 +35,41 @@ levels_img = pygame.transform.scale_by(pygame.image.load(join('assets', 'Menu', 
 mb_img = pygame.transform.scale_by(pygame.image.load(join('assets', 'Menu', 'Levels', 'MenuBox.png')), 10)
 back_img = pygame.image.load(join('assets', 'Menu', 'Buttons', 'Back.png'))
 
+def load_animations(path, direction=False):
+    animations = {}
+    sheets = listdir(path)
+    for sheet in sheets:
+        #base image
+        sprite_sheet = pygame.image.load(f'{path}/{sheet}')
+        #str
+        sheet = sheet.replace('.png', '')
+        #list of all images IN base image
+        sheet_list = []
+
+        for i in range(int(sprite_sheet.get_width() / 32)):
+            surface = pygame.Surface((32, 32), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * 32, 0, 32, 32)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sheet_list.append(pygame.transform.scale2x(surface))
+        if direction:
+            flipped_sheet_list = [pygame.transform.flip(f, True, False) for f in sheet_list]
+
+            animations[sheet + '_right'] = sheet_list
+            animations[sheet + '_left'] = flipped_sheet_list
+        else:
+            animations[sheet] = sheet_list
+
+    return animations
+
 
 class Game():
     def __init__(self):
         self.data = json.load(open(join('assets', 'Data', 'levels.json'), 'r'))
-        print(self.data)
+        self.chosen_character = 'PinkMan'
+
+        self.player = Player(0, 0, load_animations(join('assets', 'MainCharacters', self.chosen_character), True))
+        self.player_group = pygame.sprite.GroupSingle()
+        self.player_group.add(self.player)
 
         self.start()
 
@@ -53,6 +84,8 @@ class Game():
 
 
         while 1:
+            CLOCK.tick(FPS)
+
             #blit
             SCREEN.blit(bg, (-150, 0))
             SCREEN.blit(title, (int(WIDTH/2 - title.get_width()/2), int(HEIGHT/2)-150))
@@ -124,6 +157,9 @@ class Game():
             for x in range(0, ceil(WIDTH/64)*64, 64):
                 for y in range(0, ceil(HEIGHT/64)*64, 64):
                     SCREEN.blit(pygame.image.load(join('assets', 'Background', 'Brown.png')), (x, y))
+
+            self.player_group.draw(SCREEN)
+            self.player_group.update()
 
             #event
             for event in pygame.event.get():
--- Platformer(Mine)/player.py
+++ Platformer(Mine)/player.py
@@ -1,11 +1,41 @@
 import pygame
-from os import listdir
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, path):
-        super().__init__()
-        sheets = listdir(path)
+    ANIMATION_DELAY = 3
+    GRAVITY = 4
 
-        self.animations = {}
-        for sheet in sheets:
-            pass
+    def __init__(self, x: int, y: int, animations: dict):
+        super().__init__()
+
+        #animation
+        self.animations = animations
+        self.cur_animation = 'idle'
+        self.direction = 'right'
+        self.animation_count = 0
+
+        #movement
+        self.x_vel = 0
+        self.y_vel = 0
+
+        #display
+        self.image = self.animations[f'{self.cur_animation}_{self.direction}'][self.animation_count % self.ANIMATION_DELAY]
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+            self.cur_animation = 'jump'
+        if keys[pygame.K_LEFT]:
+            self.direction = 'left'
+        if keys[pygame.K_RIGHT]:
+            self.direction = 'right'
+
+        #change animation
+        if self.animation_count < len(self.animations[f'{self.cur_animation}_{self.direction}']) * self.ANIMATION_DELAY-1:
+            self.animation_count += 1
+        else:
+            self.animation_count = 0
+
+        self.image = self.animations[f'{self.cur_animation}_{self.direction}'][self.animation_count // self.ANIMATION_DELAY]
+        self.rect = self.image.get_rect()
